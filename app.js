dddc88c1530d2333f00547a77f24c0ca7e4a0aab
carly74 <calistus7004@gmail.com>
2022-08-26 20:31:13 +0200

    created a post method

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.json())
+
 const port = process.env.APP_PORT || 5000;
 
 const welcome = (req, res) => {
@@ -28,3 +30,6 @@ const userHandlers = require("./userHandler");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+
+app.post('/api/movies', movieHandlers.postMovie)
--- movieHandlers.js
+++ movieHandlers.js
@@ -28,25 +28,28 @@ const movies = [
 const database = require("./database");
 
 const getMovies = (req, res) => {
-  database.query('select * from movies')
-  .then(([movie]) => {
-    res.json(movie).send('movie data has been sent')
-  }).catch((error) => {
-    res.status(400).send('Not Found')
-  })
+  database
+    .query("select * from movies")
+    .then(([movie]) => {
+      res.json(movie).send("movie data has been sent");
+    })
+    .catch((error) => {
+      res.status(400).send("Not Found");
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
   movies.find((movie) => movie.id === id);
 
-  database.query(`select * from movies where id = ${id}`)
-  .then(([movie]) => {
-    if(!movie) return res.status(404).send('Not Found')
-  }).catch((error) => {
-    res.status(400).send('Not Found')
-  })
-
+  database
+    .query(`select * from movies where id = ${id}`)
+    .then(([movie]) => {
+      if (!movie) return res.status(404).send("Not Found");
+    })
+    .catch((error) => {
+      res.status(400).send("Not Found");
+    });
 
   // database.query(`select * from movies where id = ${id}`)
   // .then(([movie]) => {
@@ -66,9 +69,27 @@ const getMovieById = (req, res) => {
   // }
 };
 
+// Post
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+  database.query(
+    "INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+    [title, director, year, color, duration]
+  )
+  .then(([result]) => {
+    res.location(`/api/movies ${result.insertId}`).sendStatus(201)
+    console.log(result)
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error saving the movies')
+  })
+  res.send("Post route is working ✨ ");
+};
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
